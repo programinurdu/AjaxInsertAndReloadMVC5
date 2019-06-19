931786b275a36348895a84063b5eb96e8b38c257
Admin <eaak2004@hotmail.com>
2019-06-19 17:07:01 +0100

    Add project files.

---
+++ AjaxInsertAndReloadMVC5/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using AjaxInsertAndReloadMVC5.Models.DB;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace AjaxInsertAndReloadMVC5.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Student> students = new List<Student>();
+
+            using (StudentDB2Context db = new StudentDB2Context())
+            {
+                students = db.Students.ToList();
+            }
+
+            return View(students);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Index(Student student)
+        {
+            if (student.StudentId > 0)
+            {
+                using (StudentDB2Context db = new StudentDB2Context())
+                {
+                    db.Entry(student).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                }
+            }
+            else
+            {
+                using (StudentDB2Context db = new StudentDB2Context())
+                {
+                    db.Students.Add(student);
+                    db.SaveChanges();
+                }
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult ReloadStudentDetails(int studentId)
+        {
+            Student student = new Student();
+
+            using (StudentDB2Context db = new StudentDB2Context())
+            {
+                student = db.Students.Find(studentId);
+            }
+
+            // NewUpdateStudent is the partial view
+            return PartialView("NewUpdateStudent", student);
+        }
+
+
+    }
+}
---
+++ AjaxInsertAndReloadMVC5/Scripts/Site.js
@@ -0,0 +1,7 @@
+﻿var OpenDialog = function (url) {
+    //var url = "/Home/ReloadStudentDetails?StudentId=" + studentId;
+
+    $("#mymodelbody").load(url, function () {
+        $("#mymodel").modal("show");
+    })
+}
